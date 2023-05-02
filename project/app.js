c9efcc26d176ff8c08b4b5c2c5686db2e4e3307c
Saheed <saheedsh2@gmail.com>
2023-05-02 11:09:47 +0100

    first commit

--- project/app.js
+++ project/app.js
@@ -1,66 +1,69 @@
-const cards = [
+const cardArray = [
     {
         name: 'fries',
-        img: 'project/images/fries.png'
+        img: 'src/images/fries.png'
     },
     {
         name: 'cheeseburger',
-        img: 'project/images/cheeseburger.png'
+        img: 'src/images/cheeseburger.png'
     },
     {
         name: 'ice-cream',
-        img: 'project/images/ice-cream.png'
+        img: 'src/images/ice-cream.png'
     },
     {
         name: 'pizza',
-        img: 'project/images/pizza.png'
+        img: 'src/images/pizza.png'
     },
     {
         name: 'milkshake',
-        img: 'project/images/milkshake.png'
+        img: 'src/images/milkshake.png'
     },
     {
         name: 'hotdog',
-        img: 'project/images/hotdog.png'
+        img: 'src/images/hotdog.png'
     },
     {
         name: 'fries',
-        img: 'project/images/fries.png'
+        img: 'src/images/fries.png'
     },
     {
         name: 'cheeseburger',
-        img: 'project/images/cheeseburger.png'
+        img: 'src/images/cheeseburger.png'
     },
     {
         name: 'ice-cream',
-        img: 'project/images/ice-cream.png'
+        img: 'src/images/ice-cream.png'
     },
     {
         name: 'pizza',
-        img: 'project/images/pizza.png'
+        img: 'src/images/pizza.png'
     },
     {
         name: 'milkshake',
-        img: 'project/images/milkshake.png'
+        img: 'src/images/milkshake.png'
     },
     {
         name: 'hotdog',
-        img: 'project/images/hotdog.png'
+        img: 'src/images/hotdog.png'
     },
     
 ]
 
 
-cards.sort(() => 0.5 - Math.random())
-console.log(cards)
+cardArray.sort(() => 0.5 - Math.random())
+console.log(cardArray)
 let cardsChosen = []
+let cardsChosenIds = []
+let cardsWon = []
 
 const grid = document.querySelector('.grid')
+const resultDisplay = document.querySelector('#result')
 
 function createBoard(){
-    for(let i=0; i<cards.length; i++){
+    for(let i=0; i<cardArray.length; i++){
         const card = document.createElement('img')
-        card.setAttribute('src', 'images/blank.png')
+        card.setAttribute('src', 'src/images/blank.png')
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         grid.appendChild(card)
@@ -73,8 +76,49 @@ function createBoard(){
 
 function flipCard(){
     let cardId = this.getAttribute('data-id');
-    console.log(cards[cardId])
-    cardsChosen.push(cards[cardId])
+    cardsChosen.push(cardArray[cardId].name)
+    cardsChosenIds.push(cardId)
+    this.setAttribute('src', cardArray[cardId].img)
+    if(cardsChosen.length === 2){
+        setTimeout(checkForMatch,500)
+
+    }
+    console.log(cardsChosen)
+}
+
+function checkForMatch(){
+    const optionOneId = cardsChosenIds[0]
+    const optionTwoId = cardsChosenIds[1]
+
+
+    const cards = document.querySelectorAll('img')
+   if(optionOneId == optionTwoId) {
+    alert('You have clicked the same image')
+    cards[optionOneId].setAttribute('src', 'src/images/blank.png')
+    cards[optionTwoId].setAttribute('src', 'src/images/blank.png')
+   } else if(cardsChosen[0] === cardsChosen[1]){
+        alert('You have found a match!!')
+        cards[optionOneId].setAttribute('src', 'src/images/white.png')
+        cards[optionTwoId].setAttribute('src', 'src/images/white.png')
+        cards[optionOneId].removeEventListener('click', flipCard)
+        cards[optionTwoId].removeEventListener('click', flipCard)
+        cardsWon.push(cardsChosen)
+
+   } else {
+    cards[optionOneId].setAttribute('src', 'src/images/blank.png')
+    cards[optionTwoId].setAttribute('src', 'src/images/blank.png')
+    alert('Sorry, Try Aggain')
+
+   }
+   cardsChosen = []
+   cardsChosenIds = []
+   resultDisplay.textContent = cardsWon.length
+   if(cardsWon.length === cardArray.length / 2)
+   {
+    resultDisplay.textContent = 'Congratulations, You Have Won !!!!'
+   }
+   console.log(cardsChosen)
+   console.log(cardsWon)
 }
 
 createBoard()
